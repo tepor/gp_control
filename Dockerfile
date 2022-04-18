b59b36e9ff3634422f108d581035adc899b5786e
David Hedger <david@dhedger.com>
2022-04-18 02:58:47 +0000

    Added support for more gyms, including an automation attempt. Broke up the initialisation of the GPTree, aiming to do a bit of encapsulation. Added multiprocessing. Added printing GPTree to PDF.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
+# David Hedger 2022
+
 FROM ubuntu:20.04
 
-# 
+# Add some build arguments to help packages install without prompts
 ARG DEBIAN_FRONTEND=noninteractive
 ARG TZ=Etc/UTC
 
@@ -10,6 +12,11 @@ RUN apt-get update \
     git \
     fish \
     python3-pip \
+    # For visualising GP trees
+    graphviz \
+    graphviz-dev \
+    # OpenAI gym dependencies
+    swig \
     # For building Python 3.11, otherwise comment out
     build-essential \
     libssl-dev \
@@ -27,21 +34,37 @@ RUN apt-get update \
     # Clean up the cache for size reasons
     && rm -rf /var/lib/apt/lists/*
 
-# Get Python 3.11 for fun, otherwise comment out
-RUN wget -P /tmp https://www.python.org/ftp/python/3.11.0/Python-3.11.0a7.tgz \
-    && tar -xzf /tmp/Python-3.11.0a7.tgz -C /tmp \
-    && (cd /tmp/Python-3.11.0a7 \
-    && ./configure --with-ensurepip=install \
-    && make -j 8 \
-    && make altinstall)
+# # Get Python 3.11 for fun, otherwise comment out
+# RUN wget -P /tmp https://www.python.org/ftp/python/3.11.0/Python-3.11.0a7.tgz \
+#     && tar -xzf /tmp/Python-3.11.0a7.tgz -C /tmp \
+#     && (cd /tmp/Python-3.11.0a7 \
+#     && ./configure --with-ensurepip=install \
+#     && make -j 8 \
+#     && make altinstall)
+
+
+# This is broken in a very strange way
+# OpenAI gym comes with some default envs built using OpenAI MuJoCo
+# The current version of MuJoCo is 2.1
+# Versions of MuJoCo <2.0 must be licensed, but >=2.0 are free
+# Only older versions of gym(<=0.15.3) support MuJoCo 2.0 (and only up to 2.0)
+# You can go and get a free license for MuJoCo 1.5 and that is a possible workaround
+# Ultimately way too fiddly and unstable to play with right now
+
+# # Get MuJoCo for physics gyms
+# RUN wget -P /tmp https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz \
+#     && mkdir ~/.mujoco \
+#     && tar -xzf /tmp/mujoco210-linux-x86_64.tar.gz -C ~/.mujoco 
 
 # Install Python packages (3.8 packages because most don't support 3.11 yet)
 RUN pip3 install --no-cache-dir \
     numpy \
+    pathos \
     yapf \
     deap \
+    pygraphviz \
     pygame \
-    gym[classic_control]
+    gym[classic_control,box2d]
 
 # Run fish as the shell because it's nice
 CMD fish
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+# David Hedger 2022
+
 import random
 import operator
+import math
 from functools import partial
+import time
 
+# Pathos MP has better support for pickling functions inside classes
+# Saves refactoring the GPLearner to its own file and making everything global
+from pathos import multiprocessing
 import numpy as np
+import pygraphviz as pgv
 
 from deap import base, creator, gp, tools, algorithms
 import gym
+
+
+def safe_div(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 1
+
+
+def mp(fun, obj):
+    pool = multiprocessing.Pool()
+    return pool.map(fun, obj)
 
 
 class GPLearner:
@@ -14,31 +34,99 @@ class GPLearner:
         self.sim = sim
 
         self.pop_count = 300
-        self.generations = 10
+        self.generations = 40
+
+        self.choose_action_process()
+        self.create_individual_definition()
+        self.create_primitive_set()
+        self.create_toolbox()
+        self.create_logger()
+
+    def run(self):
+        # Create the initial population and run
+        self.pop = self.toolbox.population(n=self.pop_count)
+        self.pop, self.log = algorithms.eaSimple(self.pop,
+                                                 self.toolbox,
+                                                 0.5,
+                                                 0.1,
+                                                 self.generations,
+                                                 stats=self.mstats,
+                                                 halloffame=self.hof,
+                                                 verbose=True)
+
+    def choose_action_process(self):
+        # Figure out how to interact with the action space
+        space = self.sim.env.action_space
+        if type(space) == gym.spaces.discrete.Discrete:
+            actions = space.n
+            # It's difficult to know what the discrete actions map to
+            # If they're "move left, stop, move right" then we can cast all of
+            # -inf to inf and assume the controller doesn't want to move if
+            # it's near 0, but if it's in any other order then the controller
+            # will have a hard time.
+            # Beyond the case of two or three actions it should be hand-tuned
+            if actions == 2:
+                self.post_process = partial(np.digitize, bins=[0])
+            elif actions == 3:
+                self.post_process = partial(np.digitize, bins=[-0.5, 0.5])
+            else:
+                raise NotImplementedError(
+                    "Too many discrete actions, please hand-tune")
+        elif type(space) == gym.spaces.box.Box:
+            actions = space.shape[0]
+            if actions > 1:
+                # How do we do multiple continuous outputs?
+                # Can't do multiple roots in PriimitiveTree
+                # Could evolve multiple trees at the same time
+                # Cross that bridge when we find a suitable problem
+                raise NotImplementedError("Too many continuous actions")
+            else:
+                # Just pass the continuous value out with limits
+                self.post_process = np.clip(a_min=space.low, a_max=space.high)
+        else:
+            raise NotImplementedError("Unknown action type")
+
+    def create_individual_definition(self):
+        # Encapsulating the creator in a class is a bit strange since it
+        # operates in global scope
+        # Rip them from the global scope and use them locally
+        creator.create("FitnessMax", base.Fitness, weights=(1.0, ))
+        self.FitnessMax = creator.FitnessMax
+        creator.create("Individual",
+                       gp.PrimitiveTree,
+                       fitness=creator.FitnessMax)
+        self.Individual = creator.Individual
+        # This is code duplication and will make multiple classes weird
+        # I think it's worthwhile for later modularity though
+        # A caller instatiating a gptree shouldn't need to know about the other
+        # implementations, it should just be able to provide its own
+        # weights, shape, etc. without worrying about the right name
+
+    def create_primitive_set(self):
+        # Determine how many variables we can use from the environment
+        observations = self.sim.env.observation_space.shape[0]
 
         # Create the set of primitives that can become nodes in the tree
-        self.pset = gp.PrimitiveSet("MAIN", 4)
+        self.pset = gp.PrimitiveSet("MAIN", observations)
         # Input variables from the gym
-        self.pset.renameArguments(ARG0="CartPosition")
-        self.pset.renameArguments(ARG1="CartVelocity")
-        self.pset.renameArguments(ARG2="BarAngle")
-        self.pset.renameArguments(ARG3="BarVelocity")
+        self.pset.renameArguments(ARG0="CosT1")
+        self.pset.renameArguments(ARG1="SinT1")
+        self.pset.renameArguments(ARG2="CosT2")
+        self.pset.renameArguments(ARG3="SinT2")
+        self.pset.renameArguments(ARG4="VelT1")
+        self.pset.renameArguments(ARG5="VelT2")
         # Functions to apply and their arity
         self.pset.addPrimitive(operator.add, 2)
         self.pset.addPrimitive(operator.sub, 2)
         self.pset.addPrimitive(operator.mul, 2)
         self.pset.addPrimitive(operator.neg, 1)
+        # self.pset.addPrimitive(math.sin, 1)
+        # self.pset.addPrimitive(math.cos, 1)
         # Ephemeral functions are called once at the creation of their node
         self.pset.addEphemeralConstant("rand101",
                                        lambda: random.uniform(-1, 1))
 
-        # Encapsulating the creator in a class is a bit strange since it
-        # operates in global scope
-        creator.create("FitnessMax", base.Fitness, weights=(1.0, ))
-        creator.create("Individual",
-                       gp.PrimitiveTree,
-                       fitness=creator.FitnessMax)
-
+    def create_toolbox(self):
         # Lots of wrapping and partial application of functions to make
         # the lives of the learning algorithms a little easier
         self.toolbox = base.Toolbox()
@@ -48,7 +136,7 @@ class GPLearner:
                               min_=1,
                               max_=2)
         self.toolbox.register("individual", tools.initIterate,
-                              creator.Individual, self.toolbox.expr)
+                              self.Individual, self.toolbox.expr)
         self.toolbox.register("population", tools.initRepeat, list,
                               self.toolbox.individual)
         self.toolbox.register("compile", gp.compile, pset=self.pset)
@@ -62,34 +150,27 @@ class GPLearner:
                               expr=self.toolbox.expr_mut,
                               pset=self.pset)
 
+        # Add multiprocessing for parallelism
+        self.toolbox.register("map", mp)
+
         # Set some limits on the size of the trees (up to 17 is normal)
         self.toolbox.decorate(
             "mate",
-            gp.staticLimit(key=operator.attrgetter("height"), max_value=7))
+            gp.staticLimit(key=operator.attrgetter("height"), max_value=9))
         self.toolbox.decorate(
             "mutate",
-            gp.staticLimit(key=operator.attrgetter("height"), max_value=7))
+            gp.staticLimit(key=operator.attrgetter("height"), max_value=9))
 
+    def create_logger(self):
         # Some logging and statistics with the built in tools
         stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
         stats_size = tools.Statistics(len)
-        mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
-        mstats.register("avg", np.mean)
-        mstats.register("std", np.std)
-        mstats.register("min", np.min)
-        mstats.register("max", np.max)
+        self.mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+        self.mstats.register("avg", np.mean)
+        self.mstats.register("std", np.std)
+        self.mstats.register("min", np.min)
+        self.mstats.register("max", np.max)
         self.hof = tools.HallOfFame(1)
-
-        # Create the initial population and run the algorithm
-        self.pop = self.toolbox.population(n=self.pop_count)
-        self.pop, self.log = algorithms.eaSimple(self.pop,
-                                                 self.toolbox,
-                                                 0.5,
-                                                 0.1,
-                                                 self.generations,
-                                                 stats=mstats,
-                                                 halloffame=self.hof,
-                                                 verbose=True)
 
     def eval_one(self, individual):
         # Compile the individual to callable function
@@ -98,11 +179,14 @@ class GPLearner:
         # Ooh sneaky functional stuff
         # I was inspired by the partial application used in the DEAP docs
         def shim(ind_func, action_space, obs):
-            prediction = ind_func(obs[0], obs[1], obs[2], obs[3])
+            prediction = ind_func(obs[0], obs[1], obs[2], obs[3], obs[4],
+                                  obs[5])
             # Need to figure out how to interact with action spaces to do
             # this on the fly
             if prediction > 0.5:
                 return 1
+            if prediction < -0.5:
+                return -1
             else:
                 return 0
 
@@ -113,14 +197,32 @@ class GPLearner:
 
         return (reward, )
 
+    @staticmethod
+    def visualise(individual):
+        nodes, edges, labels = gp.graph(individual)
+        print(labels)
+        print(edges)
+
+        g = pgv.AGraph()
+        g.add_nodes_from(nodes)
+        g.add_edges_from(edges)
+        g.layout(prog="dot")
+
+        for node in nodes:
+            n = g.get_node(node)
+            n.attr["label"] = labels[node]
+
+        g.draw("tree.pdf")
+
 
 class Simulation:
 
     def __init__(self):
-        self.env = gym.make("CartPole-v1")
+        self.env = gym.make("Acrobot-v1")
         self.episodes = 100
         self.max_timesteps = 300
         self.planner = None
+        self.wall_start = time.time()
 
     def set_planner(self, planner):
         self.planner = planner
@@ -166,8 +268,10 @@ class Simulation:
 
         self.env.close()
 
+        # wall_stop = time.time()
+        # elapsed = wall_stop - self.wall_start
         # print(
-        #     f"Simlation complete. Average reward: {run_reward/self.episodes}")
+        #     f"Simlation complete. Average reward: {run_reward/self.episodes}. Elapsed: {elapsed:.1f}s")
 
         return run_reward / self.episodes
 
@@ -186,7 +290,5 @@ class Simulation:
 if __name__ == "__main__":
     sim = Simulation()
     gptree = GPLearner(sim)
-
-    nodes, edges, labels = gp.graph(gptree.hof[0])
-    print(labels)
-    print(edges)
+    gptree.run()
+    gptree.visualise(gptree.hof[0])
